c401b64ef6311ee955634b6593b80d3d9ec4ddc4
Iris Liu <68066244+Iris-liu-creator@users.noreply.github.com>
2026-05-12 13:45:07 +1000

    Refine SugarCRM subscriptions staging model

--- models/staging/sugarcrm/stg_sugarcrm_subscriptions.sql
+++ models/staging/sugarcrm/stg_sugarcrm_subscriptions.sql
@@ -1,8 +1,10 @@
 select
     id as subscription_id,
     name as subscription_name,
-    status as status,
-    date_entered as created_at,
-    date_modified as updated_at,
-    deleted as is_deleted
+    status,
+    stage,
+    date_modified::timestamp as updated_at,
+    deleted::boolean as is_deleted,
+    _fivetran_synced
+
 from {{ source('sugarcrm', 'subsc_subscriptions') }}
