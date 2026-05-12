b876e5c129716be2c525aa904ec73c95dae34e58
Iris Liu <68066244+Iris-liu-creator@users.noreply.github.com>
2026-05-12 12:02:49 +1000

    Update SugarCRM subscriptions staging model

--- models/staging/sugarcrm/stg_sugarcrm_subscriptions.sql
+++ models/staging/sugarcrm/stg_sugarcrm_subscriptions.sql
@@ -1,3 +1,7 @@
 select
-    *
+    id as subscription_id,
+    name as subscription_name,
+    date_entered as created_at,
+    date_modified as updated_at,
+    deleted as is_deleted
 from {{ source('sugarcrm', 'subsc_subscriptions') }}
